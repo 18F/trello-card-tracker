e03ee773d9e8da2010c37b14c74959ae63ae176a
Laura Gerhardt <laura.gerhardt@gsa.gov>
2016-02-11 11:34:29 -0500

    build passing tests that are not really writing to trello

--- app/stagemanager.js
+++ app/stagemanager.js
@@ -11,7 +11,7 @@ util.inherits(StageManager, TrelloSuper);
 
 var method = StageManager.prototype;
 
-method.checkLists = function(readStages){
+method.checkLists = function(readStages, callback){
 var checked = [];
 	this.t.get("/1/boards/"+this.board+"/lists", function(err, data){
 
@@ -20,10 +20,8 @@ var checked = [];
 		_un.each(readStages, function(stage){
 			checked.push({"stage": stage["name"], "built": _un.contains(lists, stage["name"])});
 		});
-		return checked;
-		// console.log(checked);
+		callback(checked);
 	});
-	// return;
 };
 
 method.makeAdditionalLists = function(checkedList){
@@ -31,16 +29,14 @@ method.makeAdditionalLists = function(checkedList){
 	_un.each(checkedList, function(list, i){
 		if (!list["built"]){
 			var postList = {name: list["stage"], idBoard: classThis.board, pos: i+1};
-			console.log(postList);
 			classThis.t.post("1/lists", postList, function(err,data){
 				if (err) throw err;
-				console.log(data);
 			});
 		}
 	});
 };
 
-method.deleteUnusedStages = function(){
+method.closeUnusedStages = function(){
 	classThis = this;
 	stages = _un.pluck(classThis.relevantStage, 'name');
 	classThis.t.get("/1/boards/"+classThis.board+"/lists", function(err, data){
@@ -49,10 +45,9 @@ method.deleteUnusedStages = function(){
 			if (!(_un.contains(stages, trelloList["name"]))){
 				classThis.t.get("/1/lists/"+trelloList["id"]+"/cards", function(err, listData){
 					if (err) throw err;
-					// console.log(listData);
 					if (listData.length === 0){
 						classThis.t.put("/1/list/"+trelloList["id"]+"/closed", {value: true}, function(e, success){
-							console.log(success);
+							console.log("Lists Closed");
 						})
 					}
 				});
@@ -68,19 +63,8 @@ method.deleteUnusedStages = function(){
 method.run = function(){
 	this.stages = this.readYaml();
 	this.relevantStage = this.stages.stages[0].substages;
-	this.checkedList = this.checkLists(this.relevantStage);
-	// this.checkedList = [ { stage: 'IAA', built: false },
-  // { stage: 'Workshop Prep', built: false },
-  // { stage: 'Workshop', built: false },
-  // { stage: 'RFP Writing', built: false },
-  // { stage: 'CO Review', built: false },
-  // { stage: 'Client Review', built: false },
-  // { stage: 'Award Finalized', built: false },
-  // { stage: 'Solicitation', built: false },
-  // { stage: 'Vendor Review', built: false } ];
-	this.makeAdditionalLists(this.checkedList);
-	// console.log(checked);
-	this.deleteUnusedStages();
+	this.checkLists(this.relevantStage, this.makeAdditionalLists);
+	this.closeUnusedStages();
 }
 
 
---
+++ test/test-stage-manager.coffee
@@ -0,0 +1,74 @@
+expect = require('chai').expect
+_un = require("underscore")
+app = require('../app')
+
+board = process.env.TRELLO_BPA_TEST_BOARD
+mockfile = './test/mockfile.yaml'
+
+stubbed_list = ["Kanbanian", "Kanbanian-Dos"]
+
+make_lists = [ { stage: 'Kanbanian', built: false },
+{ stage: 'Kanbanian-Dos', built: false }]
+
+expectedStageObject =
+  stages: [ {
+    name: 'Pre-Award'
+    substages: [
+      {
+        name: 'IAA'
+        expected_time: 5
+      }
+      {
+        name: 'Workshop Prep'
+        expected_time: 10
+      }
+    ]
+  } ]
+
+stages = expectedStageObject.stages[0].substages
+console.log stages
+stageMgr = new app.StageManager(mockfile, board)
+
+describe 'stageManager', ->
+  describe '.readYAML', ->
+    it 'maps a yaml to an object with a stages key', ->
+      yamlObject = stageMgr.readYaml()
+      expect(yamlObject).to.eql(expectedStageObject)
+      return
+    return
+
+  describe '.checkLists', -> # skipping because testing async fxn callback...boooo
+    it 'checks which stages in an object of stages are in a trello board', ->
+      expected_object = ["Kanbanian", "Kanbanian-Dos"]
+      stageMgr.checkLists(stubbed_list, testCallback)
+      testCallback = (checkedList) ->
+        stgs = _un.pluck(checkedList, 'name')
+        expect(checkedListg).to.eql expected_list
+        return
+      # expect(result_object).to.include.members(expected_object);
+      return
+    return
+
+  describe '.makeAdditionalLists', ->
+    it 'given a list of objects that include the name of unbuilt lists, it makes additional lists in trello', ->
+      stageMgr.makeAdditionalLists(make_lists)
+      stageMgr.t.get '/1/boards/' + stageMgr.board + '/lists', (err, data) ->
+        if err
+          throw err
+        boardLists = _un.pluck(data, 'name')
+        expect(boardLists).to.include.members(stubbed_list)
+        return
+      return
+    return
+
+  describe '.closeUnusedStages', ->
+    it 'closes a lists given a list if it is not in a stages object where there are no cards', ->
+      stageMgr.closeUnusedStages(stages)
+      stageMgr.t.get '/1/boards/' + stageMgr.board + '/lists', (err, data) ->
+        if err
+          throw err
+        shouldbeClosed = _un.findWhere(data, {name: 'Should be closed board'})
+        expect(shouldbeClosed["closed"]).to.be.true
+        # console.log data
+      return
+  return
