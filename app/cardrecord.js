2bbfeda05a230fe34a5fe2f46f0f6ab174629ce0
Laura Gerhardt <laura.gerhardt@gsa.gov>
2016-02-23 16:49:19 -0500

    delete card creator methods from old card recorder

--- app/cardrecord.js
+++ app/cardrecord.js
@@ -12,43 +12,6 @@ util.inherits(CardRecorder, TrelloSuper);
 
 var method = CardRecorder.prototype;
 
-method.createOrders = function(orderFile){
-	classThis = this;
-	var orders = yaml.safeLoad(fs.readFileSync(orderFile, 'utf8'));
-	_un.each(orders.orders, function(order){
-		classThis.createCard(order);
-	});
-}
-
-method.createCard = function(order){
-	var description = this.descriptionMaker(order);
-	var cardName = order["project"] +" - "+order["order"];
-
-	if (order["due"]){
-		var due = order["due"];
-	} else {
-		var due = null;
-	}
-	classThis = this;
-	this.getListIDbyName(order["stage"], function(listID){
-		var cardInfo = {"name": cardName,
-										"desc": description,
-										"idList": listID,
-										"due": due
-									};
-		classThis.t.post('1/cards/', cardInfo, function(err, data){
-			if (err) {throw err};
-			console.log(data);
-		});
-
-	});
-}
-
-method.descriptionMaker = function(order){
-	return "Project: {p}\nAgency: {a}\nSubAgency: {sub}\nTrello Board: {tb}"
-		.supplant({p: order["project"], a: order["agency"], sub: order["subagency"], tb: order["trello"]});
-}
-
 method.addComment = function(message, cardID){
 	this.t.post("1/cards/"+cardID+"/actions/comments", {text: message}, function(err, data){
 		if (err) {throw err};
