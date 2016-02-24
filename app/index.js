1dec22c4663051b0c21f1c7bc290dc735c088ede
Greg Walker <michael.walker@gsa.gov>
2016-02-23 18:20:26 -0600

    Add dotenv

--- app/index.js
+++ app/index.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 exports.StageManager = require("./stagemanager.js");
 exports.CardRecorder = require("./cardrecord.js");
 exports.CardCreator = require("./cardcreator.js");
