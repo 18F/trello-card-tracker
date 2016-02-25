5c4ae62f21615d6783137a19533624fa691dd39d
Laura Gerhardt <laura.gerhardt@gsa.gov>
2016-02-25 15:16:43 -0500

    get writing current comments, need to fix older posts

--- app/cardrecord.js
+++ app/cardrecord.js
@@ -19,66 +19,87 @@ method.run = function(){
 	classThis.t.get('/1/boards/'+this.board+'/cards', {actions: ["createCard", "updateCard"]}, function(err, cards){
 		if (err) {throw err};
 		_un.each(cards, function(card){
-			classThis.deleteCurrentComment(card["id"]);
-			var now = moment();
-			hasBeenUpdated = _un.findWhere(card["actions"], {type:"updateCard"});
-			if ((now.diff(moment(card.actions[0].date), 'hours') < 24 ) || (!hasBeenUpdated)){
-				console.log("recent");
-				// classThis.findLastMoves(card["id"]);
-			} else {
-				console.log("new phase");
-				 Q.all([classThis.getListNameByID(card["idList"]), classThis.findLastMoves(card["id"]), classThis.getLastList(card.actions[0]["id"])])
-				 .then(function(values){
-					 var phase = values[2];
-				 	classThis.compileCommentArtifact(card["id"], values, phase);
-				 });
-			}
+			classThis.deleteCurrentComment(card["id"]).done(function(d){
+				var now = moment();
+				var hasBeenUpdated = _un.findWhere(card["actions"], {type:"updateCard"});
+				var daysSinceUpdate = now.diff(moment(card.actions[0].date), 'days');
+				if ((daysSinceUpdate > 0 ) || (!hasBeenUpdated)){
+					console.log("Write Current Comment");
+					classThis.getListNameByID(card["idList"])
+						.then(function(listName){
+							classThis.compileCommentArtifact(card["id"], listName, "Current", card.actions[0].date, moment().format());
+						});
+				} else {
+					console.log("Write New Phase");
+					 Q.all([classThis.getListNameByID(card["idList"]), classThis.getLastList(card.actions[0]["id"])])
+					 .then(function(lists){
+						 var listName = lists[0]
+						 var lastPhase = list[1];
+						classThis.compileCommentArtifact(card["id"], listName, lastPhase, lists, card["actions"][1]["date"], card["actions"][0]["date"]);
+					 });
+				}
+			});
 		});
 	});
 }
 
-method.compileCommentArtifact = function(cardID, asyncValues, phase){
-	console.log(asyncValues);
-	var listName = asyncValues[0];
-		var stage = _un.findWhere(classThis.Stages, {name: listName});
-		var expectedTime = stage["expected_time"];
-		var frmDate = asyncValues[1][0];
-		var toDate = asyncValues[1][1];
-		var diffArray = classThis.calculateDateDifference(expectedTime, frmDate, toDate);
-		var differenceFromExpected = diffArray[0]
-		var timeTaken = diffArray[1];
-		var comment = classThis.buildComment(differenceFromExpected, expectedTime, frmDate, toDate, phase, timeTaken);
-		console.log(comment);
-		classThis.addComment(comment, cardID);
+method.deleteCurrentComment = function(cardID){
+	var deferred = Q.defer();
+	classThis.t.get('/1/cards/'+cardID+'/actions', {filter:'commentCard'}, function(err, comments){
+		if(err) {deferred.reject(new Error(err));};
+		_un.each(comments,function(c){
+			if (c.data.text.indexOf("**Current Stage:**") != -1){
+				var currentCommentID = c["id"];
+			}
+			if(currentCommentID){
+				classThis.t.del('/1/actions/'+currentCommentID, function(err, data){
+					if(err) {deferred.reject(new Error(err));};
+					deferred.resolve(data);
+				});
+			} else {
+				deferred.resolve("no delete");
+			}
+		});
+	});
+		return deferred.promise;
+}
 
-};
 
 
 method.getLastList = function(updateListID){
 	var deferred = Q.defer();
 	this.t.get('/1/actions/'+updateListID, function(err, action){
 		if(err) {deferred.reject(new Error(err));};
-		console.log("get last list");
-		console.log(action["data"]["listBefore"]);
 		deferred.resolve(action["data"]["listBefore"]["name"]);
 	});
 	return deferred.promise;
 }
 
-method.deleteCurrentComment = function(cardID){
-	classThis = this;
-	this.t.get('/1/cards/'+cardID+'/actions', {filter:'commentCard'}, function(err, comments){
-		_un.each(comments,function(c){
-			if (c.data.text.indexOf("**Current Stage:**") != -1){
-				var currentCommentID = c["id"];
-			}
-			if(currentCommentID){
-				classThis.t.delete('/1/cards/'+cardID+'/actions/'+currentCommentID+'/comments', function(err, data){
-					if (err) throw err;
-				});
-			}
-		});
-	});
+//Run function to build a comment
+method.compileCommentArtifact = function(cardID, listName, phase, fromDate, toDate){
+		var stage = _un.findWhere(classThis.Stages, {name: listName});
+		var expectedTime = stage["expected_time"];
+		var diffArray = classThis.calculateDateDifference(expectedTime, fromDate, toDate);
+		var differenceFromExpected = diffArray[0];
+		var timeTaken = diffArray[1];
+		var comment = classThis.buildComment(differenceFromExpected, expectedTime, fromDate, toDate, phase, timeTaken);
+		console.log(comment);
+		classThis.addComment(comment, cardID);
+
+};
+
+method.calculateDateDifference = function(expected, lastMove, recentMove){
+	var fromDate = new Date(lastMove);
+	var toDate = new Date(recentMove);
+	var diffDays = instadate.differenceInWorkDays(fromDate, toDate);
+	return [diffDays - expected, diffDays]
+}
+
+method.buildComment = function(dateDiff, expected, lastMove, recentMove, lastList, actual){
+	formatDiff = (dateDiff < 0)? "**"+dateDiff+" days**" :"`+"+dateDiff+" days`"
+	msg = "**{l} Stage:** {d}. *{f} - {t}*.\n Expected days: {e} days. Actual Days spent: {a}."
+		.supplant({l: lastList, d: formatDiff, e: expected, a: actual, f: moment(lastMove).format("L"), t: moment(recentMove).format("L")});
+	return msg;
 }
 
 method.addComment = function(message, cardID){
@@ -88,41 +109,24 @@ method.addComment = function(message, cardID){
 	 });
 }
 
-method.calculateDateDifference = function(expected, lastMove, recentMove){
-	var fromDate = new Date(lastMove);
-	var toDate = new Date(recentMove);
-	var diffDays = instadate.differenceInWorkDays(fromDate, toDate);
-	return [diffDays - expected, diffDays]
-}
 
 
 
-method.findLastMoves = function(cardID){
-	var deferred = Q.defer();
-	this.t.get("/1/cards/"+cardID+"/actions", {"filter": ["updateCard","createCard"]}, function(err, actions){
-		if(err) {deferred.reject(new Error(err));};
-		updatedCards = _un.where(actions, {type: 'updateCard'});
-		if (updatedCards > 1){
-			var fromAction = updatedCards[1];
-			var toAction = updateCards[0];
-		} else {
-			var fromAction = _un.findWhere(actions, {type:"createCard"});
-			var toAction = _un.findWhere(actions, {type:"updateCard"});
-		}
-		deferred.resolve([fromAction["date"], toAction["date"]]); // From to To Date
-	});
- return deferred.promise;
-}
+
+// method.findLastMoves = function(cardActions){
+// 		updatedCards = _un.where(actions, {type: 'updateCard'});
+// 		if (updatedCards > 1){
+// 			var fromAction = updatedCards[1];
+// 			var toAction = updateCards[0];
+// 		} else {
+// 			var fromAction = _un.findWhere(actions, {type:"createCard"});
+// 			var toAction = _un.findWhere(actions, {type:"updateCard"});
+// 		}
+// 	return [fromAction["date"], toAction["date"]]; // From to To Date
+// }
 
 
-method.buildComment = function(dateDiff, expected, lastMove, recentMove, lastList, actual){
-	fromDate = moment(lastMove).format("L");
-	toDate = moment(recentMove).format("L");
-	formatDiff = (dateDiff < 0)? "**"+dateDiff+" days**" :"`+"+dateDiff+" days`"
-	msg = "**{l} Stage:** {d}. *{f} - {t}*.\n Expected days: {e} days. Actual Days spent: {a}."
-		.supplant({l: lastList, d: formatDiff, e: expected, a: actual, f: fromDate, t: toDate });
-	return msg;
-}
+
 
 
 module.exports = CardRecorder;
