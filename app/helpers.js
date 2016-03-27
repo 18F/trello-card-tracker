f76753adc75ed7a0461e2d6cd06cb80ebeb45362
gracegit <gracegoogoo@gmail.com>
2016-03-27 17:53:08 -0400

    refactor helpers js to es6 classes

--- app/helpers.js
+++ app/helpers.js
@@ -5,62 +5,64 @@ Trello = require("node-trello");
 
 var method = TrelloSuper.prototype;
 
-function TrelloSuper(yaml_file, board) {
+class TrelloSuper{
+  constructor(yaml_file, board) {
     this.yaml_file = yaml_file;
     this.board = board;
     this.t = new Trello(process.env.TRELLO_API_KEY, process.env.TRELLO_API_TOK);
     this.lists_url = "/1/boards/"+board+"/lists";
 }
 
-method.readYaml = function() {
+  readYaml() {
     try {
     	var doc = yaml.safeLoad(fs.readFileSync(this.yaml_file, 'utf8'));
       return doc;
     } catch (e){
       return null;
     }
-};
-
-method.getPreAward = function(){
-  stages = this.readYaml(this.yaml_file);
-  if(stages) {
-    return stages.stages[0].substages;
-  } else {
-    return [ ];
   }
-}
 
-method.getListIDbyName = function(name, callback){
-  var deferred = Q.defer();
-  var find = true;
-  if (!name) {
-    var find = false;
+  getPreAward(){
+    stages = this.readYaml(this.yaml_file);
+    if(stages) {
+      return stages.stages[0].substages;
+    } else {
+      return [ ];
+    }
   }
-  this.t.get(this.lists_url, function(e, data){
-    if(e) {
-      return deferred.reject(e);
-    }
-    if(find){
-      var list = _un.findWhere(data, {"name": name});
-    }
-    if (!find || !list){ //Add it to the first list if there is not one
-      var list = data[0];
-    }
-    deferred.resolve(list["id"]);
-  });
 
-  return deferred.promise;
-}
+  getListIDbyName(name, callback){
+    var deferred = Q.defer();
+    var find = true;
+    if (!name) {
+      var find = false;
+    }
+    this.t.get(this.lists_url, function(e, data){
+      if(e) {
+        return deferred.reject(e);
+      }
+      if(find){
+        var list = _un.findWhere(data, {"name": name});
+      }
+      if (!find || !list){ //Add it to the first list if there is not one
+        var list = data[0];
+      }
+      deferred.resolve(list["id"]);
+    });
 
-method.getListNameByID = function(listID){
-  var deferred = Q.defer();
-  this.t.get('/1/lists/'+listID, function(err, list){
-    if(err) {
-      return deferred.reject(new Error(err));
-    };
-		deferred.resolve(list["name"]);
-  });
-  return deferred.promise;
+    return deferred.promise;
+  }
+
+  getListNameByID(listID){
+    var deferred = Q.defer();
+    this.t.get('/1/lists/'+listID, function(err, list){
+      if(err) {
+        return deferred.reject(new Error(err));
+      };
+  		deferred.resolve(list["name"]);
+    });
+    return deferred.promise;
+  }
 }
 
 String.prototype.supplant = function (o) {
