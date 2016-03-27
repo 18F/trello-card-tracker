0db2d4af8f59862bdff930da20d90f410936f377
gracegit <gracegoogoo@gmail.com>
2016-03-27 18:05:22 -0400

    refactor to es6 cardcreator class

--- app/cardcreator.js
+++ app/cardcreator.js
@@ -3,54 +3,46 @@ var util = require('util');
 var yaml = require('js-yaml');
 Q = require('q');
 
-var classThis;
-function CardCreator(yaml_file, board){
-	TrelloSuper.call(this, yaml_file, board);
-		classThis = this;
-}
-
-util.inherits(CardCreator, TrelloSuper);
-
-module.exports = CardCreator;
-
-var method = CardCreator.prototype;
-
-method.createOrders = function(orderFile){
-	var orders = yaml.safeLoad(fs.readFileSync(orderFile, 'utf8'));
-	var promises = [ ];
-	_un.each(orders.orders, function(order){
-		promises.push(classThis.createCard(order));
-	});
-	return Q.all(promises);
-}
-
-method.createCard = function(order){
-	var deferred = Q.defer();
-	var description = this.descriptionMaker(order);
-	var cardName = order["project"] +" - "+order["order"];
-	var due = null;
-
-	if (order["due"]){
-		due = order["due"];
+class CardCreator extends TrelloSuper{
+	createOrders(orderFile){
+		var orders = yaml.safeLoad(fs.readFileSync(orderFile, 'utf8'));
+		var promises = [ ];
+		_un.each(orders.orders, function(order){
+			promises.push(this.createCard(order));
+		});
+		return Q.all(promises);
 	}
 
-	classThis.getListIDbyName(order["stage"]).then(function(listID){
-		var cardInfo = {"name": cardName,
-										"desc": description,
-										"idList": listID,
-										"due": due
-									};
-		classThis.t.post('1/cards/', cardInfo, function(err, data){
-			if(err) {
-				return deferred.reject(new Error(err));
-			}
-			deferred.resolve(data);
+	createCard(order){
+		var deferred = Q.defer();
+		var description = this.descriptionMaker(order);
+		var cardName = order["project"] +" - "+order["order"];
+		var due = null;
+
+		if (order["due"]){
+			due = order["due"];
+		}
+
+		this.getListIDbyName(order["stage"]).then(function(listID){
+			var cardInfo = {"name": cardName,
+											"desc": description,
+											"idList": listID,
+											"due": due
+										};
+			this.t.post('1/cards/', cardInfo, function(err, data){
+				if(err) {
+					return deferred.reject(new Error(err));
+				}
+				deferred.resolve(data);
+			});
 		});
-	});
-	return deferred.promise;
+		return deferred.promise;
+	}
+
+	descriptionMaker(order){
+		return "Project: {p}\nAgency: {a}\nSubAgency: {sub}\nTrello Board: {tb}"
+			.supplant({p: order["project"], a: order["agency"], sub: order["subagency"], tb: order["trello"]});
+	}
 }
 
-method.descriptionMaker = function(order){
-	return "Project: {p}\nAgency: {a}\nSubAgency: {sub}\nTrello Board: {tb}"
-		.supplant({p: order["project"], a: order["agency"], sub: order["subagency"], tb: order["trello"]});
-}
+module.exports = CardCreator;
