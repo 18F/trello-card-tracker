bd9aff0f1e3f2f4b2f86bff00c7c3220b9860aa0
gracegit <gracegoogoo@gmail.com>
2016-03-27 17:57:56 -0400

    refactore to es6 stagemanager class

--- app/stagemanager.js
+++ app/stagemanager.js
@@ -3,121 +3,116 @@ TrelloSuper = require("./helpers.js");
 util = require('util');
 Q = require('q');
 
-var classThis;
-function StageManager(yaml_file, board){
-	TrelloSuper.call(this, yaml_file, board);
-	this.Stages = this.getPreAward();
-	classThis = this;
-}
 
-util.inherits(StageManager, TrelloSuper);
+class StageManager extends TrelloSuper{
+	constructor(){
+		this.Stages = this.getPreAward();
+	}
 
-var method = StageManager.prototype;
+	run(){
+		var deferred = Q.defer();
+		this.getStageandBoard()
+		.then(this.checkLists)
+	  .then(this.makeAdditionalLists)
+		.then(this.getStageandBoard().then(this.closeUnusedStages))
+		.then(this.getStageandBoard().then(this.orderLists))
+		.then(function() { deferred.resolve("complete") })
+		.catch(function(e) {
+			deferred.reject(e);
+	  });
 
-method.run = function(){
-	var deferred = Q.defer();
-	this.getStageandBoard()
-	.then(this.checkLists)
-  .then(this.makeAdditionalLists)
-	.then(this.getStageandBoard().then(this.closeUnusedStages))
-	.then(this.getStageandBoard().then(this.orderLists))
-	.then(function() { deferred.resolve("complete") })
-	.catch(function(e) {
-		deferred.reject(e);
-  });
+		return deferred.promise;
+	}
 
-	return deferred.promise;
-}
+	getStageandBoard(){
+		var deferred = Q.defer();
+		this.t.get(this.lists_url, function(err, data){
+			if(err) {deferred.reject(new Error(err));};
+			deferred.resolve([classThis.Stages, data]);
+		});
+		return deferred.promise;
+	}
 
-method.getStageandBoard = function(){
-	var deferred = Q.defer();
-	this.t.get(this.lists_url, function(err, data){
-		if(err) {deferred.reject(new Error(err));};
-		deferred.resolve([classThis.Stages, data]);
-	});
-	return deferred.promise;
+	checkLists(data){ //PASS STAGES ASYNC
+		var checked = [];
+		lists = _un.pluck(data[1], 'name');
+		_un.each(data[0], function(stage){
+			checked.push({"stage": stage["name"], "built": _un.contains(lists, stage["name"])});
+		});
+		return checked;
+	}
 
-}
+	makeAdditionalLists(checkedList){
+		var deferred = Q.defer();
+		var all = [ ];
+		var newLists = [ ];
 
-method.checkLists = function(data){ //PASS STAGES ASYNC
-	var checked = [];
-	lists = _un.pluck(data[1], 'name');
-	_un.each(data[0], function(stage){
-		checked.push({"stage": stage["name"], "built": _un.contains(lists, stage["name"])});
-	});
-	return checked;
-};
+		_un.each(checkedList, function(list, i){
+			var listDefer = Q.defer();
+			all.push(listDefer.promise);
+			if (!list["built"]){
+				var postList = {name: list["stage"], idBoard: classThis.board, pos: i+1};
+				classThis.t.post("1/lists", postList, function(err,data){
+					if (err) {
+						listDefer.reject(err);
+					}
+					newLists.push(data);
+					listDefer.resolve();
+				});
+			}
+		});
 
-method.makeAdditionalLists = function(checkedList){
-	var deferred = Q.defer();
-	var all = [ ];
-	var newLists = [ ];
+		Q.all(all).then(function() {
+			deferred.resolve(newLists);
+		}).catch(function(e) {
+			deferred.reject(e);
+		})
 
-	_un.each(checkedList, function(list, i){
-		var listDefer = Q.defer();
-		all.push(listDefer.promise);
-		if (!list["built"]){
-			var postList = {name: list["stage"], idBoard: classThis.board, pos: i+1};
-			classThis.t.post("1/lists", postList, function(err,data){
-				if (err) {
-					listDefer.reject(err);
-				}
-				newLists.push(data);
-				listDefer.resolve();
-			});
-		}
-	});
+		return deferred.promise;
+	}
 
-	Q.all(all).then(function() {
-		deferred.resolve(newLists);
-	}).catch(function(e) {
-		deferred.reject(e);
-	})
+	closeUnusedStages(data, callback){
+		stages = _un.pluck(data[0], 'name'); //Grab stage names
+		// For each list
+		_un.each(data[1], function(trelloList){
+			if (!(_un.contains(stages, trelloList["name"]))){
+				classThis.getListCards(trelloList["id"], function(d){
+					classThis.closeList(d, trelloList["id"], callback)
+				});
+			};
+		});
+		return;
+	}
 
-	return deferred.promise;
-};
-
-method.closeUnusedStages = function(data, callback){
-	stages = _un.pluck(data[0], 'name'); //Grab stage names
-	// For each list
-	_un.each(data[1], function(trelloList){
-		if (!(_un.contains(stages, trelloList["name"]))){
-			classThis.getListCards(trelloList["id"], function(d){
-				classThis.closeList(d, trelloList["id"], callback)
-			});
-		};
-	});
-	return;
-}
-
-method.getListCards = function(trelloID, callback){
-	this.t.get("/1/lists/"+trelloID+"/cards", function(err, data){
-		if(err) throw err;
-		callback(data);
-	});
-}
-
-method.closeList = function(listData, trelloID, callback){
-	if (listData.length === 0){
-		classThis.t.put("/1/list/"+trelloID+"/closed", {value: true}, function(e, success){
-			if (e) {throw e};
-			callback(success);
+	getListCards(trelloID, callback){
+		this.t.get("/1/lists/"+trelloID+"/cards", function(err, data){
+			if(err) throw err;
+			callback(data);
 		});
 	}
-}
 
-method.orderLists = function(data){
-	var position = 0;
-	_un.each(data[0], function(stage, i){
-		appropriateList = _un.findWhere(data[1], {name: stage["name"]})
-		if(appropriateList) {
-			listID = appropriateList["id"];
-			classThis.t.put("1/lists/"+listID+"/pos", {value: position}, function(e, data) {
+	closeList(listData, trelloID, callback){
+		if (listData.length === 0){
+			classThis.t.put("/1/list/"+trelloID+"/closed", {value: true}, function(e, success){
 				if (e) {throw e};
+				callback(success);
 			});
-			position++;
 		}
-	});
+	}
+
+	orderLists(data){
+		var position = 0;
+		_un.each(data[0], function(stage, i){
+			appropriateList = _un.findWhere(data[1], {name: stage["name"]})
+			if(appropriateList) {
+				listID = appropriateList["id"];
+				classThis.t.put("1/lists/"+listID+"/pos", {value: position}, function(e, data) {
+					if (e) {throw e};
+				});
+				position++;
+			}
+		});
+	}
 }
 
 module.exports = StageManager;
